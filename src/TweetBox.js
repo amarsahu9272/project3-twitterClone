e80e163bc8eebf8a282c6c75b36d5329af43ca47
Anil Singh <anil927a@gmail.com>
2023-01-18 22:40:58 +0530

    twitterClone/localStroage

---
+++ src/TweetBox.js
@@ -0,0 +1,80 @@
+import { Avatar, Button } from "@mui/material";
+import React, { useState } from "react";
+import "./TweetBox.css";
+
+function TweetBox() {
+  // displayName: "", username: "", verified: "" ,text:"",image:"",avatar:""
+  const [formValues, setFormValues] = useState({
+    displayName: "",
+    username: "",
+    verified: "",
+    text: "",
+    image: "",
+    avatar: "",
+  });
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    // db.collection("posts").add({
+    //   displayName: "Rafeh Qazi",
+    //   username: "cleverqazi",
+    //   verified: true,
+    //   text: tweetMessage,
+    //   image: tweetImage,
+    //   avatar:
+    //     "https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
+    // });
+    setFormValues({
+      ...formValues,
+      displayName: "Rafeh Qazi",
+      username: "cleverqazi",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png",
+    });
+    // setTweetMessage("");
+    // setTweetImage("");
+    let post = JSON.parse(localStorage.getItem("tweet")) || [];
+    post.push(formValues);
+    localStorage.setItem("tweet", JSON.stringify(post));
+    // setTweetMessage("");
+    // setTweetImage("");
+  };
+  return (
+    <div className="tweetBox">
+      <form onSubmit={sendTweet}>
+        <div className="tweetBox__input">
+          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
