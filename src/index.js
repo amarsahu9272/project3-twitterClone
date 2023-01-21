411f6318e51e978956c0906477923da58d2f0113
Anil Singh <anil927a@gmail.com>
2023-01-21 23:57:54 +0530

    authorization created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <SignInPage />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <SignUpPage />,
+//   },
+// ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot> */}
   </React.StrictMode>
 );
-
-
